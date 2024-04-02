db934c029bc992bdddd26f8ada0b88faa229f8df
Gabasha <gabashidze05@gmail.com>
2024-04-02 16:58:40 +0400

    !

---
+++ app.js
@@ -0,0 +1,25 @@
+let productname="vashli";
+let productprice=3;
+let productquantity=10;
+let productweight=0.5;
+let productfinalprice= productprice*productquantity;
+let productfinalweight= productquantity*productweight
+console.log("product=",productname)
+console.log("product final price=",productfinalprice)
+console.log("product weight=",productfinalweight+"გ")
+
+
+let myName="giorgi";
+let myHobby="candle making";
+let yearOfBirth=2005;
+let currentYear=2024;
+let currentage= currentYear-yearOfBirth
+
+console.log(`my name is ${myName}`)
+console.log(`my hobby ${myHobby}`)
+console.log(`my age at this moment ${currentage}`)
+
+
+
+
+
